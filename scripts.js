32ebaec7b7ed40cf3391b10f117535742200e1fb
Danilo Teofilo <danigolaseu@gmail.com>
2022-08-09 23:37:51 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -101,3 +101,62 @@
 
 // let cadastro = (nome, idade, altura) => ("nome: " + nome + ", sua idade é " + idade + " anos e sua altura é " + altura);
 // console.log(cadastro("danilo", 30, 1.96))
+
+
+// let pessoas = [
+//     {nome: "Carlos" , 
+//     dataDeNascimento: "1990-06-29" , 
+//     aprovado: true , 
+//     categorias: ["A"]},
+
+
+//     {nome: "Ricardo" , 
+//     dataDeNascimento: "1976-04-23" , 
+//     aprovado: true , 
+//     categorias: ["A" , "B" , "C"] },
+
+  
+//     {nome: "Rafael" , 
+//     dataDeNascimento: "1995-05-01" , 
+//     aprovado: true, 
+//     categorias: ["A" , "B"]},
+  
+  
+  
+//     {nome: "Larissa" , 
+//     dataDeNascimento: "2000-03-04" , 
+//     aprovado: true , 
+//     categorias: ["A"]}
+// ]
+
+// let data = new Date();
+// let dia = String(data.getDate()).padStart(2, "0");
+// let mes = String(data.getMonth() + 1).padStart(2, "0");
+// let ano = data.getFullYear() - 18;
+// let dataAtual = (ano + "-" + mes + "-" + dia);
+
+// function listarAprovadosECategoria() {
+//     for (let i = 0; i < pessoas.length; i++){
+//         const categorias = pessoas[i].categorias;
+//         const eMaiorDeIdade = pessoas[i].dataDeNascimento <= dataAtual
+//         const eAprovado = pessoas[i].aprovado
+    
+//         if(eAprovado && eMaiorDeIdade && categorias.includes("B")){
+//             console.log(pessoas[i].nome)
+//         }
+
+        // for (let j = 0; j < pessoas[i].categorias.length; j++) {
+            
+        //     const categorias = pessoas[i].categorias[j];
+        //     const eMaiorDeIdade = pessoas[i].dataDeNascimento <= dataAtual
+        //     const eAprovado = pessoas[i].aprovado
+        
+        //     if(eAprovado && eMaiorDeIdade && categorias == "B"){
+        //         console.log(pessoas[i].nome)
+        //     }
+     
+        // }
+//     }
+// }
+    
+    // listarAprovadosECategoria()
